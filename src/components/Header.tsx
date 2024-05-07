ef2c5073c10fd864804c9fe15cdba4ab40452a3c
Ibrahim Office PC <142000003+brhmkhn4@users.noreply.github.com>
2024-05-07 18:16:07 +0500

    header added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import ModeToggle from "../components/ui/ModeToggle"
+import { ModeToggle } from "../components/ui/ModeToggle"
 
 export default function Header() {
   return (
-    <div className="h-5 border-2">
+    <div className=" flex justify-end h-auto px-5 py-3">
         < ModeToggle />
-    </div>
+     </div>
   )
 }
