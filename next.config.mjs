39fc33b50472ec6998624927c2046e7469057de8
Ibrahim Office PC <142000003+brhmkhn4@users.noreply.github.com>
2024-05-11 11:45:59 +0500

    commit before making changes in tailwind config file, as tailwind utility classes are not working

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'image1.jdomni.in',
+            port: '',
+            pathname: '/banner/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
--- src/components/card.new.tsx
+++ src/components/card.new.tsx
@@ -3,9 +3,9 @@ export default function Card(props:any) {
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
           <img
-            src=""
+            src="https://image1.jdomni.in/banner/13062021/0A/52/CC/1AF5FC422867D96E06C4B7BD69_1623557926542.png"
             alt="wheat flour grinding"
-            className="w-full h-64 object-cover"
+            className="aspect-[2/3] h-80 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
           />
           <div className="p-6 text-center">
             <h3 className="text-xl font-medium text-gray-800 mb-2">
